83c228adaed0c2b1659a44913bf731908592b49a
Md.Munaour Mehtab <engmunacse@gmail.com>
2017-06-11 23:19:59 +0600

    init

---
+++ XunitDemo.Test/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XunitDemo.Test
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void ShouldAdd()
+        {
+            var sut = new Calculator();
+            var result = sut.Add(1,2);
+
+            Assert.Equal(3, result);
+        }
+    }
+}
